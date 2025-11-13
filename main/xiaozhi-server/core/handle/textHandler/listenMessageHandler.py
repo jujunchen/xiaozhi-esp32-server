a1c1a419bb2a885c3ff39cc1445a3c4f34bf274b
jujunchen <1913210361@qq.com>
2025-11-13 23:58:21 +0800

    conn.client_have_voice = True 会把一小段的音频也识别成文字

--- main/xiaozhi-server/core/handle/textHandler/listenMessageHandler.py
+++ main/xiaozhi-server/core/handle/textHandler/listenMessageHandler.py
@@ -24,7 +24,7 @@ class ListenTextMessageHandler(TextMessageHandler):
                 f"客户端拾音模式：{conn.client_listen_mode}"
             )
         if msg_json["state"] == "start":
-            conn.client_have_voice = True
+            conn.client_have_voice = False
             conn.client_voice_stop = False
         elif msg_json["state"] == "stop":
             conn.client_have_voice = True
