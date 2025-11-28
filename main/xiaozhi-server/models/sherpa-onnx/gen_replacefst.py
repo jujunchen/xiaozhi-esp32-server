d75ffddfb46ae2b6168e3e1f8b944252479748a4
jujunchen <jujunchen>
2025-11-28 22:34:37 +0800

    sherpa-onnx 多音字

---
+++ main/xiaozhi-server/models/sherpa-onnx/gen_replacefst.py
@@ -0,0 +1,32 @@
+import pynini
+from pynini.lib import utf8, byte
+from pynini import cdrewrite
+
+sigma = utf8.VALID_UTF8_CHAR.star
+
+rule1 = pynini.cross('pu3ji4chan2si4', '普济禅寺')
+rule10 = pynini.cross('pu3ji4can2si4', '普济禅寺')
+rule11 = pynini.cross('pu3ji4can2shi4', '普济禅寺')
+rule12 = pynini.cross('pu3ji4chan2shi4', '普济禅寺')
+rule23 = pynini.cross('bu3ji4chan2si4', '普济禅寺')
+rule24 = pynini.cross('bu3ji4can2si4', '普济禅寺')
+rule25 = pynini.cross('bu3ji4can2shi4', '普济禅寺')
+rule26 = pynini.cross('bu3jin4chan2shi4', '普济禅寺')
+rule27 = pynini.cross('bu3jin4chan2si4', '普济禅寺')
+rule28 = pynini.cross('bu3jin4can2si4', '普济禅寺')
+rule29 = pynini.cross('bu3jin4can2shi4', '普济禅寺')
+rule30 = pynini.cross('bu3jin4chan2shi4', '普济禅寺')
+rule2 = pynini.cross('fa2yu3can2si4', '法雨禅寺')
+rule20 = pynini.cross('fa2yu3chan2si4', '法雨禅寺')
+rule21 = pynini.cross('fa2yu3chan2shi4', '法雨禅寺')
+rule3 = pynini.cross('zhou1shan1', '舟山')
+rule31 = pynini.cross('zou1san1', '舟山')
+rule32 = pynini.cross('zhou1san1', '舟山')
+rule33 = pynini.cross('zou1shan1', '舟山')
+
+
+
+rule = (rule1 | rule10 | rule11 | rule12 | rule23 | rule24 | rule25 | rule26 | rule27 | rule28 | rule29 | rule30 | rule2 | rule20 | rule21).optimize()
+rule = cdrewrite(rule, "", "", sigma)
+
+rule.write('models/sherpa-onnx/replace.fst')
